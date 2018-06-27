9d3564054a6c36b6b296c42c679c045b59e532d5
Boutaudn <boutaudn@gmail.com>
2018-06-27 19:50:46 -0400

    agregada navbar(mientras display none), colores y pagina de login, trabajando en reconocimiento de login y password

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 /* Holiii acá va tu código también */
+const inputMail = document.getElementById('mail').value;
+const inputPass = document.getElementById('password').value;
+const btnLogin = document.getElementById('btn-login');
+
+btnLogin.addEventListener('click', () => {
+    if(inputPass.length > 8){
+    console.log('correcto')
+    }else{
+    console.log('incorrecto')
+  }
+})
